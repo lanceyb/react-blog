7e7d82352a2e2fabad31c9daf123f8b1b6c14365
lance <lance.zyb@gmail.com>
2015-11-26 21:01:47 +0800

    evn config

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'React Blog',
+  description: 'My Blog website generator for React.js web applications.'
+};
+
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>Coming soon.</p>
+      </div>
+    );
+  }
+
+}
+
